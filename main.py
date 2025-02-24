10afee6b634b8aeee03b3b877698bb642768a7b5
thiagoregueira <thiago.regueira@yahoo.com.br>
2025-02-24 09:22:47 -0300

    refactor: Atualizar arquivo Excel para concurso 2832 e remover concurso 2829

--- main.py
+++ main.py
@@ -18,8 +18,8 @@ st.set_page_config(
 with open('styles.css') as f:  # noqa: PLW1514
     st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
 
-# lêr o arquivo excel mega_sena_asloterias_ate_concurso_2829_sorteio.xlsx
-# df_excel = pd.read_excel('mega_sena_asloterias_ate_concurso_2829_sorteio.xlsx')
+# lêr o arquivo excel mega_sena_asloterias_ate_concurso_2832_sorteio.xlsx
+# df_excel = pd.read_excel('mega_sena_asloterias_ate_concurso_2832_sorteio.xlsx')
 
 # criar um banco de dados com os dados de df_excel
 db = sqlite3.connect('mega_sena.db')
